3ba9297f0f4bb444862f5c08ca30a8e8b810ce7f
DarkArtek <darkartek@gmail.com>
2020-05-15 10:13:24 +0200

    i18n Project

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject chocobo "0.1"
-    :description "It provides the berries you need to modify a Chocobo color in FFXIV"
+    :description "Elenca le bacche richieste per colorare il Chocobo in FFXIV"
     :url "https://github.com/DarkArtek/ChocoColor"
     :license {:name "LGPL"
               :url "https://www.gnu.org/licenses/lgpl-3.0.txt"}
